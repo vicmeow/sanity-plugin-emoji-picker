6bab0770d2cb8af74015f18c10e5d14436ff967b
vicbergquist <victoria.bergquist@gmail.com>
2019-06-17 11:59:26 +0200

    rename example schema to emoji

--- example/schemas/schema.js
+++ example/schemas/schema.js
@@ -2,11 +2,11 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-import name from './name'
+import emoji from './emoji'
 
 export default createSchema({
   name: 'default',
   types: schemaTypes.concat([
-    name
+    emoji
   ])
 })
